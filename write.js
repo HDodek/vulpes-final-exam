c35389e1d4806580f5137508b2385fc1d15efaa2
Dorottya Hidas <dorottya.hidas@gmail.com>
2016-03-16 10:19:15 +0100

    Add write.js

---
+++ write.js
@@ -0,0 +1,16 @@
+'use stict';
+
+var fs = require('fs');
+var fileName = require('./apple.txt');
+var animals = JSON.stringify({mammal: 'cat', reptile: 'crocodile'});
+
+
+function writeToFile(cb, animals) {
+	setTimeout(function() {
+		cb(animals);
+	}, 2000);
+}
+
+writeToFile(function(fileName, animal) {
+	fs.writeFile('./apple.txt', animals);
+});
